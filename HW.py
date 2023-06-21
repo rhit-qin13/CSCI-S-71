92ee4a70a22d04a3b64c7d3b28e24e205563a194
rhit-qin13 <112580159+rhit-qin13@users.noreply.github.com>
2023-06-20 21:53:35 -0400

    Update HW.py

--- HW.py
+++ HW.py
@@ -1,1 +1,8 @@
-print("Hello World!");
+def hello_world(name):
+    print(f" Hello, {name}! ")
+
+if __name__ == "__main__":
+    print("Please enter your name:")
+    name = input()
+    hello_world(name)
+
